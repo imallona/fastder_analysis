21763a65dd7804a1369d7ff93a40b3ea72966c69
Tom Kitak <tomaz.kitak@gmail.com>
2026-02-20 18:06:27 +0100

    More clean up

--- workflow/scripts/force_strand.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Force all features to positive (+) strand in GFF3/GTF files
-# Usage: bash force_strand.sh input.gff3 output.gff3
-
-awk 'BEGIN{FS=OFS="\t"} /^#/ {print; next} {$7="+"; print}' "$1" > "$2"
