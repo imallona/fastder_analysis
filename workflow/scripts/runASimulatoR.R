e02451d1770f1763ef80847075dce5b5124da5bb
Tom Kitak <tomaz.kitak@gmail.com>
2026-02-20 21:14:23 +0100

    Fix, empty group is actually needed in ASim

--- workflow/scripts/runASimulatoR.R
+++ workflow/scripts/runASimulatoR.R
@@ -19,7 +19,7 @@ simulate_alternative_splicing(
   input_dir             = input_dir,
   outdir                = outdir,
   event_probs           = event_probs,
-  num_reps              = c(1),
+  num_reps              = c(1, 0),
   seed                  = seed,
   ncores                = ncores,
   seq_depth             = seq_depth,
